712e15a2875f2a9355fbfe08801d00f9e438b26b
Rouzaire <ylann.rouzaire@epfl.ch>
2020-02-27 11:56:49 +0100

    adds "cross" sub-dataset

--- dataset.py
+++ dataset.py
@@ -132,13 +132,16 @@ def get_normalized_dataset(dataset, p=0, d=0, seed=0):
         tr = torchvision.datasets.ImageFolder('~/.torchvision/datasets/catdog', transform=transform)
         x, y = dataset_to_tensors(list(tr))
         x = center_normalize(x)
-    elif dataset in ['stripe', 'sphere']:
+    elif dataset in ['stripe', 'sphere' , 'cross']:
         x = torch.randn(2 * p, d, dtype=torch.float64)
         if dataset == 'stripe':
             y = (x[:, 0] > -0.3) * (x[:, 0] < 1.18549)
         if dataset == 'sphere':
             r = x.norm(dim=1)
             y = (r > d**0.5)
+        if dataset == 'cross':
+            threshold_x = 0.0  ; threshold_y = 0.0 
+            y = (x[:, 0] > threshold_x) * (x[:, 1] > threshold_y ) + (x[:, 0] < threshold_x) * (x[:, 1] < threshold_y )
         y = 2 * y - 1
         tr = [(x, y.item()) for x, y in zip(x, y)]
         x, y = dataset_to_tensors(tr)
