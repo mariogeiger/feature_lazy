1a6306baee5787ec3178f0fa41323e7a87b18773
Mario Geiger <geiger.mario@gmail.com>
2020-05-13 12:53:14 +0200

    ode

--- dynamics.py
+++ dynamics.py
@@ -273,3 +273,65 @@ def train_kernel(ktrtr, ytr, tau, loss_prim, max_dgrad=math.inf, max_dout=math.i
 
         # 5 - If yes, compute the new output and gradient
         grad = new_grad
+
+
+def ode_evolve(var0, grad_fn, max_dgrad):
+    var = copy.deepcopy(var0)
+
+    dt = 1
+    current_dt = 0
+    step_change_dt = 0
+
+    t = 0
+
+    grad = grad_fn(var)
+    dgrad = 0
+
+    for step in itertools.count():
+
+        state = {
+            'step': step,
+            't': t,
+            'dt': current_dt,
+            'dgrad': dgrad,
+        }
+
+        yield state, var, grad
+
+        if torch.isnan(grad).any():
+            break
+
+        # 1 - Save current state
+        state = copy.deepcopy((var, t))
+
+        while True:
+            # 2 - Make a tentative step
+            var.add_(dt * grad)
+            t += dt
+            current_dt = dt
+
+            # 3 - Check if the step is small enough
+            new_grad = grad_fn(var)
+
+            if torch.isnan(new_grad).any():
+                break
+
+            if grad.norm() == 0 or new_grad.norm() == 0:
+                dgrad = 0
+            else:
+                dgrad = (grad - new_grad).norm().pow(2).div(grad.norm() * new_grad.norm()).item()
+
+            if dgrad < max_dgrad:
+                if dgrad < 0.5 * max_dgrad:
+                    dt *= 1.1
+                break
+
+            # 4 - If not, reset and retry
+            dt /= 10
+
+            print("[{} +{}] [dt={:.1e} dgrad={:.1e}]".format(step, step - step_change_dt, dt, dgrad), flush=True)
+            step_change_dt = step
+            var, t = copy.deepcopy(state)
+
+        # 5 - If yes, compute the new output and gradient
+        grad = new_grad
