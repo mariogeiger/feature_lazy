3be3c2d3988c4953c8a3705ebb1897aeefcf5e20
Mario Geiger <geiger.mario@gmail.com>
2020-10-11 18:47:24 +0200

    jamming

--- jamming.py
+++ jamming.py
@@ -13,12 +13,15 @@ def main():
     parser.add_argument("--alpha", type=float)
     parser.add_argument("--h", type=int)
 
+    parser.add_argument("--seed_init", type=int)
+    parser.add_argument("--ptr", type=int)
+
     args = parser.parse_args()
 
     h = args.h
 
     while True:
-        param = (('h', h), ('alpha', args.alpha))
+        param = (('h', h), ('alpha', args.alpha), ('seed_init', args.seed_init), ('ptr', args.ptr))
         data = exec_blocking(args.log_dir, args.cmd, param)
         dyn = data['regular']['dynamics']
         if dyn[-1]['train']['nd'] > 0:
