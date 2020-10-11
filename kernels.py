f203373d45f60e359fa9d04a9d568d0e9dc17201
Mario Geiger <geiger.mario@gmail.com>
2020-10-11 19:19:01 +0200

    gradient

--- kernels.py
+++ kernels.py
@@ -2,11 +2,28 @@
 """
 Computes the Gram matrix of a given model
 """
+import torch
+
+
+def gradient(outputs, inputs, grad_outputs=None, retain_graph=None, create_graph=False):
+    '''
+    Compute the gradient of `outputs` with respect to `inputs`
+    gradient(x.sum(), x)
+    gradient((x * y).sum(), [x, y])
+    '''
+    if torch.is_tensor(inputs):
+        inputs = [inputs]
+    else:
+        inputs = list(inputs)
+    grads = torch.autograd.grad(outputs, inputs, grad_outputs,
+                                allow_unused=True,
+                                retain_graph=retain_graph,
+                                create_graph=create_graph)
+    grads = [x if x is not None else torch.zeros_like(y) for x, y in zip(grads, inputs)]
+    return torch.cat([x.contiguous().view(-1) for x in grads])
 
 
 def compute_kernels(f, xtr, xte, parameters=None):
-    from hessian import gradient
-
     if parameters is None:
         parameters = list(f.parameters())
 
