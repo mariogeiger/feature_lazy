23b0a7fd3bbfc38ec01bb3e29f62d1ba8458b028
Mario Geiger <geiger.mario@gmail.com>
2020-10-25 10:24:28 +0100

    print

--- jamming.py
+++ jamming.py
@@ -22,6 +22,7 @@ def main():
     while True:
         jammed = True
         for seed in args.seed_init:
+            print('try h={}, seed={}'.format(h, seed))
             r = exec_blocking(args.log_dir, args.cmd, (('h', h), ('alpha', args.alpha), ('seed_init', seed)))
             if r['regular']['dynamics'][-1]['train']['nd'] == 0:
                 jammed = False
