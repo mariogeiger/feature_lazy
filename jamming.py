c00ba68b5ca11fea25acfed6327d74a5e25fd586
Mario Geiger <geiger.mario@gmail.com>
2020-10-26 09:16:32 +0100

    fix

--- jamming.py
+++ jamming.py
@@ -2,6 +2,7 @@
 import argparse
 
 from grid.exec import exec_blocking
+from grid import load
 
 
 def main():
@@ -24,6 +25,10 @@ def main():
         jammed = True
         for seed in args.seed_init:
             print('try h={}, seed={}'.format(h, seed))
+            rs = load(args.log_dir, pred_args=lambda a: a.h == h and a.alpha == args.alpha and a.seed_init == seed)
+            if any(r['regular']['dynamics'][-1]['train']['nd'] == 0 for r in rs):
+                jammed = False
+                break
             r = exec_blocking(args.log_dir, args.cmd, (('h', h), ('alpha', args.alpha), ('seed_init', seed), ('wall', args.wall)))
             if r['regular']['dynamics'][-1]['train']['nd'] == 0:
                 jammed = False
