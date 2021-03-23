7442828566b923f2d43f12ca62165a60f0d2610d
Mario Geiger <geiger.mario@gmail.com>
2021-03-23 11:54:51 +0100

    nseed

--- exp/dt_over_alpha.py
+++ exp/dt_over_alpha.py
@@ -30,15 +30,18 @@ def main():
     parser.add_argument("--base", type=float)
     parser.add_argument("--tol", type=int, default=5)
 
+    parser.add_argument("--nseed", type=int)
+
     args = parser.parse_args()
 
     runs = []
 
-    for i in range(args.i_start, args.i_end):
-        for j in range(args.j_start, args.j_end):
-            alpha = round_mantissa(args.base ** (i + j), args.tol)
-            dt = round_mantissa(args.base ** (-i), args.tol)
-            runs += [exec_one(args.log_dir, args.cmd, (('alpha', alpha), (args.tlike, dt)))]
+    for s in range(args.nseed):
+        for i in range(args.i_start, args.i_end):
+            for j in range(args.j_start, args.j_end):
+                alpha = round_mantissa(args.base ** (i + j), args.tol)
+                dt = round_mantissa(args.base ** (-i), args.tol)
+                runs += [exec_one(args.log_dir, args.cmd, (('alpha', alpha), (args.tlike, dt), ('seed_init', s)))]
 
     for r in runs:
         r(False)  # wait but don't load
