77aded8ad7ee9d574c13da2b47f0764b68ac11f5
Mario Geiger <geiger.mario@gmail.com>
2021-10-10 19:04:40 +0200

    fix

--- sgd_jax.py
+++ sgd_jax.py
@@ -236,7 +236,7 @@ def train(
     ckpt_loss = l0 * jnp.concatenate([ckpt_loss, jnp.arange(2**-9, 1, 2**-9), 1 - ckpt_loss[::-1]])
 
     ckpt_loss_grad = 2.0**jnp.arange(-20, -2, 0.5)
-    ckpt_loss_grad = jnp.concatenate([ckpt_loss_grad, jnp.arange(2**-2, 1.0, 2**-4)])
+    ckpt_loss_grad = l0 * jnp.concatenate([ckpt_loss_grad, jnp.arange(2**-2, 1.0, 2**-4)])
 
     target_loss = l0
     current_loss = l0
